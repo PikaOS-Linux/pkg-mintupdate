4c314a647dfcad802ff73403200ee5aa002be459
ward <ward@pop-os.localdomain>
2023-04-26 15:28:29 +0300

    Upgrade to lunar

--- main.sh
+++ main.sh
@@ -4,13 +4,16 @@ add-apt-repository https://ppa.pika-os.com
 add-apt-repository ppa:pikaos/pika
 add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
+git clone https://github.com/linuxmint/mintupdate
+cp -rvf ./debian ./mintsystem
 cd ./mintupdate
+patch -Np1 -i ../patches/pika-adapt.diff
 
 # Get build deps
 apt-get build-dep ./ -y
 
 # Build package
-dpkg-buildpackage
+dpkg-buildpackage --no-sign
 
 # Move the debs to output
 cd ../
